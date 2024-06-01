f102ca9348f28a38410b0c6cbc06edaa009518aa
Pintu kumar <pintu1012kumar@gmail.com>
2024-06-01 18:30:16 +0530

    massage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navabar/Navbar";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
 
 const App = () =>{
   return(
     <div>
        <Navbar/>
        <Hero/>
+       <About/>
+       <Services/>
     </div>
   )
 }
